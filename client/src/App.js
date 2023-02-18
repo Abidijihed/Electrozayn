166036b405d53e21efdb0fc55da6876b05e33dca
Abidijihed <abidij55@gmail.com>
2023-02-18 17:06:12 +0100

    QUER CODE ADDED

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-
+import React, { useEffect, useState } from 'react';
+import QRCode from 'qrcode.react';
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    setUrl('https://abdelwahebbouden.com');
+  }, []);
+  const handleRedirect = () => {
+    window.location.href = url;
+  };
   return (
     <div className="App">
       <header className="App-header">
- 
+      <div>
+      <QRCode value={url} onClick={handleRedirect} />
+    </div>
       </header>
     </div>
   );
