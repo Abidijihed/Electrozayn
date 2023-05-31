2df9f1c589d5e638db8b03236f7fa58b8c7e6905
Abidijihed <abidij55@gmail.com>
2023-05-31 12:23:21 +0100

    save

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,14 +2,14 @@ import { Typography, makeStyles, Link } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { BsFillTelephoneFill } from "react-icons/bs";
-import { AiOutlineMail,AiFillHome } from "react-icons/ai";
+import { AiOutlineMail, AiFillHome } from "react-icons/ai";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',  // Update the flexDirection to 'row'
+    alignItems: 'flex-start', // Align items to the start of the row
+    justifyContent: 'flex-start', // Align items to the start of the row
     marginTop: theme.spacing(10),
     paddingTop: theme.spacing(2),
     paddingBottom: theme.spacing(2),
@@ -18,6 +18,9 @@ const useStyles = makeStyles((theme) => ({
   link: {
     margin: theme.spacing(1),
   },
+  contactInfo: {
+    marginRight: theme.spacing(2), // Add margin to separate contact info from links
+  },
 }));
 
 function Footer() {
@@ -25,15 +28,26 @@ function Footer() {
 
   return (
     <div className={classes.root}>
-    
+      <div className={classes.contactInfo}>
+        <Typography variant="body1" gutterBottom>
+          <BsFillTelephoneFill /> +216 22 181 411
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          <BsFillTelephoneFill /> +216 55 181 417
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          <AiOutlineMail /> aymensatellite@gmail.com
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          <AiFillHome /> 1 Rue de Pirée rue d'Athènes Tunis 1000
+        </Typography>
+      </div>
       <Typography>
-
         <Link href="#" className={classes.link}>About Us</Link>
         <Link href="#" className={classes.link}>Privacy Policy</Link>
         <Link href="#" className={classes.link}>Terms of Use</Link>
         <Link href="#" className={classes.link}>Contact Us</Link>
       </Typography>
-
       <Typography variant="caption">&copy; 2023 Electrozayn.com, Inc. or its affiliates</Typography>
     </div>
   );
