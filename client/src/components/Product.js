77e3c127db7dfd30c4616bd7413eaf1e80f8aea0
Abidijihed <abidij55@gmail.com>
2023-05-31 10:00:27 +0100

    save

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -10,42 +10,50 @@ import {
 import { Button } from 'react-bootstrap';
 import AddProductModal from './UpdateModal';
 import axios from 'axios';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
+    flex: '0 0 33.33%',
+    marginBottom: '1rem',
+    display: 'flex',
+    flexDirection: 'column',
   },
   media: {
     height: 140,
   },
+  promoPrice: {
+    color: 'green',
+  },
+  originalPrice: {
+    color: 'red',
+    textDecoration: 'line-through',
+  },
+  shopIcon: {
+    float: 'right',
+    marginTop: '-1rem',
+    marginRight: '-1rem',
+  },
 });
 
-function ProductCard({data}) {
+function ProductCard({ data }) {
   const [openAddProductModal, setOpenAddProductModal] = useState(false);
-  const [user,setUser]=useState([])
-  
+  const [user, setUser] = useState([]);
 
-    useEffect(() => {
-      const user_id = localStorage.getItem("id");
-      axios.get("https://www.electrozayn.com/api/user/getone/"+ user_id)
-        .then((res) => {
-         
-          setUser(res.data);
-        });
-    }, []);
+  useEffect(() => {
+    const user_id = localStorage.getItem('id');
+    axios.get('https://www.electrozayn.com/api/user/getone/' + user_id).then((res) => {
+      setUser(res.data);
+    });
+  }, []);
+
   const classes = useStyles();
-
- 
 
   return (
     <Card className={classes.root}>
-        
       <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={data.product_image}
-          title={data.product_name}
-        />
+        <CardMedia className={classes.media} image={data.product_image} title={data.product_name} />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {data.product_name}
@@ -54,14 +62,16 @@ function ProductCard({data}) {
             {data.description}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Original Price: {data.Origin_price} TND
+            <span className={classes.originalPrice}>Original Price: {data.Origin_price} TND</span>
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             Quantity: {data.quantity}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Promo Price: {data.Promo_price?data.Promo_price:null} TND
-          </Typography>
+          {data.Promo_price && (
+            <Typography variant="body2" color="textSecondary" component="p">
+              <span className={classes.promoPrice}>Promo Price: {data.Promo_price} TND</span>
+            </Typography>
+          )}
           <Typography variant="body2" color="textSecondary" component="p">
             Reference: {data.reference}
           </Typography>
@@ -73,23 +83,26 @@ function ProductCard({data}) {
           </Typography>
         </CardContent>
         <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        {user.map((el)=>{
-            return(
-             <>
-            {el.role === "admin" ?<button onClick={() => setOpenAddProductModal(true)}>update</button>:null}
-             </>
-            )
-        })}
-      
+          {user.map((el) => {
+            return (
+              <>
+                {el.role === 'admin' ? (
+                  <Button onClick={() => setOpenAddProductModal(true)}>Update</Button>
+                ) : null}
+              </>
+            );
+          })}
+        </div>
+        <div className={classes.shopIcon}>
+          <FaShoppingCart />
+        </div>
+      </CardActionArea>
       <AddProductModal
         open={openAddProductModal}
         handleClose={() => setOpenAddProductModal(false)}
         product={data}
         id={data.id}
       />
-
-    </div>
-      </CardActionArea>
     </Card>
   );
 }
