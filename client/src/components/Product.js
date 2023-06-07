68ba61a197a54b6000259ef5c58470b617e4ff27
Abidijihed <abidij55@gmail.com>
2023-06-07 15:26:28 +0100

    s

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -58,9 +58,9 @@ function ProductCard({ data ,getlengthShop}) {
     axios
       .get('https://www.electrozayn.com/api/get_all_shopcard/card')
       .then((res) => {
-        const product = res.data.find((product) => product.products_id=== data.id);
+        const product = res.data.find((product) => product.products_i === data.id);
         localStorage.setItem("shop",res.data.length)
-        console.log(product.check_add_or_not)
+        console.log(product)
         if (product) {
           setChek(product.check_add_or_not);
         }
