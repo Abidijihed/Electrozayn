addfbd572d38d6fd1530255293b8c691af2ce3d4
Abidijihed <abidij55@gmail.com>
2023-06-10 11:14:58 +0100

    s

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import {
   Card,
   CardContent,
@@ -10,36 +10,36 @@ import {
   Typography,
   Button,
   CircularProgress,
-} from '@material-ui/core';
-import CheckoutValidation from './CheckoutValidation';
-import axios from 'axios';
+} from "@material-ui/core";
+import CheckoutValidation from "./CheckoutValidation";
+import axios from "axios";
 import { TiDelete } from "react-icons/ti";
 const useStyles = makeStyles({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    padding: '1rem',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    padding: "1rem",
   },
   card: {
-    display: 'flex',
-    alignItems: 'center',
-    marginBottom: '1rem',
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "1rem",
   },
   image: {
-    marginRight: '1rem',
-    width: '50px',
-    height: '50px',
-    objectFit: 'cover',
+    marginRight: "1rem",
+    width: "50px",
+    height: "50px",
+    objectFit: "cover",
   },
   total: {
-    marginTop: '1rem',
-    fontWeight: 'bold',
+    marginTop: "1rem",
+    fontWeight: "bold",
   },
   deleteIcon: {
-    marginRight: '13px',
-    fontSize: '25px',
-    cursor:"pointer"
+    marginRight: "13px",
+    fontSize: "25px",
+    cursor: "pointer",
   },
 });
 
@@ -47,27 +47,30 @@ export default function Checkout({ getlengthShop }) {
   const classes = useStyles();
   const [openCheckoutValidation, setOpenCheckoutValidation] = useState(false);
   const [products, setProducts] = useState([]);
-  const [paymentMethod, setPaymentMethod] = useState('pay_on_delivery');
+  const [paymentMethod, setPaymentMethod] = useState("pay_on_delivery");
   const [total, setTotal] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('https://www.electrozayn.com/api/get_product/card').then((res) => {
-      const productsWithQuantity = res.data.map((product) => ({
-        ...product,
-        quantity: 1, // Initialize quantity as 1 for each product
-      }));
-      setProducts(productsWithQuantity);
-      setIsLoading(false);
-    });
+    axios
+      .get("https://www.electrozayn.com/api/get_product/card")
+      .then((res) => {
+        const productsWithQuantity = res.data.map((product) => ({
+          ...product,
+          quantity: 1, // Initialize quantity as 1 for each product
+        }));
+        setProducts(productsWithQuantity);
+        setIsLoading(false);
+      });
     getlengthShop();
   }, [products]);
   const handleUpdate = (id) => {
-   axios.put(`https://www.electrozayn.com/api/update/shop_card/${id}`)
-   .then((res)=>{
-    setProducts(products)
-   })
-  }
+    axios
+      .put(`https://www.electrozayn.com/api/update/shop_card/${id}`)
+      .then((res) => {
+        setProducts(products);
+      });
+  };
 
   useEffect(() => {
     const totalPrice = products.reduce(
@@ -84,17 +87,18 @@ export default function Checkout({ getlengthShop }) {
     if (isLoading || products.length === 0) {
       return 0;
     }
-    if (paymentMethod === 'pay_on_delivery') {
+    if (paymentMethod === "pay_on_delivery") {
       return total + 7;
     }
     return total;
   };
 
-
   const incrementQuantity = (productId) => {
     setProducts((prevProducts) =>
       prevProducts.map((product) =>
-        product.id === productId ? { ...product, quantity: product.quantity + 1 } : product
+        product.id === productId
+          ? { ...product, quantity: product.quantity + 1 }
+          : product
       )
     );
   };
@@ -111,11 +115,13 @@ export default function Checkout({ getlengthShop }) {
 
   const updateQuantityOnServer = (productId, quantity) => {
     axios
-      .put(`https://www.electrozayn.com/api/update_quantity/${productId}`, { quantity })
+      .put(`https://www.electrozayn.com/api/update_quantity/${productId}`, {
+        quantity,
+      })
       .then((res) => {
-        toast.success('Success Notification !', {
-          position: toast.POSITION.TOP_RIGHT
-      });// Quantity Updated
+        toast.success("Success Notification !", {
+          position: toast.POSITION.TOP_RIGHT,
+        }); // Quantity Updated
       })
       .catch((error) => {
         console.error(error);
@@ -137,27 +143,41 @@ export default function Checkout({ getlengthShop }) {
       ) : (
         products.map((product) => (
           <Card key={product.id} className={classes.card}>
-            <img src={product.product_image} alt={product.product_name} className={classes.image} />
+            <img
+              src={product.product_image}
+              alt={product.product_name}
+              className={classes.image}
+            />
             <CardContent>
-              <Typography variant="subtitle1">{product.product_name}</Typography>
+              <Typography variant="subtitle1">
+                {product.product_name}
+              </Typography>
               <Typography variant="caption">{product.reference}</Typography>
               <Typography variant="body2" color="textSecondary" component="p">
                 Promo Price: {product.Promo_price} TND
               </Typography>
 
-              <div style={{ display: 'flex' }}>
+              <div style={{ display: "flex" }}>
                 <Button onClick={() => incrementQuantity(product.id)}>+</Button>
                 <Typography>{product.quantity}</Typography>
                 <Button onClick={() => decrementQuantity(product.id)}>-</Button>
               </div>
               <Typography variant="body2" color="black" component="h3">
-
-                Total: {Number(product.Promo_price) * Number(product.quantity)} TND
+                Total: {Number(product.Promo_price) * Number(product.quantity)}{" "}
+                TND
               </Typography>
             </CardContent>
-            <div style={{ display: 'flex', justifyContent: 'flex-end', color: "red" }}  >
-
-              <TiDelete className={classes.deleteIcon} onClick={() => handleUpdate(product.id)} />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                color: "red",
+              }}
+            >
+              <TiDelete
+                className={classes.deleteIcon}
+                onClick={() => handleUpdate(product.id)}
+              />
             </div>
           </Card>
         ))
@@ -165,7 +185,7 @@ export default function Checkout({ getlengthShop }) {
       <FormControlLabel
         control={
           <Checkbox
-            checked={paymentMethod === 'pay_on_delivery'}
+            checked={paymentMethod === "pay_on_delivery"}
             onChange={handlePaymentMethodChange}
             value="pay_on_delivery"
           />
@@ -175,7 +195,7 @@ export default function Checkout({ getlengthShop }) {
       <FormControlLabel
         control={
           <Checkbox
-            checked={paymentMethod === 'pay_with_card'}
+            checked={paymentMethod === "pay_with_card"}
             onChange={handlePaymentMethodChange}
             value="pay_with_card"
           />
@@ -185,15 +205,18 @@ export default function Checkout({ getlengthShop }) {
       <Typography variant="h6" className={classes.total}>
         Total Price: {handleTotal()} TND
       </Typography>
-      <Button variant="contained" color="primary" onClick={() => setOpenCheckoutValidation(true)}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setOpenCheckoutValidation(true)}
+      >
         Validate Order
       </Button>
       <CheckoutValidation
         open={openCheckoutValidation}
         handleClose={() => setOpenCheckoutValidation(false)}
-      //  total={total}
-      //  products={products}
-
+        total={total}
+        products={products}
       />
     </div>
   );
