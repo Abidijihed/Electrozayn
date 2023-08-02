70962b9e855a780956f776659cbeb251376e35c7
Abidijihed <abidij55@gmail.com>
2023-08-02 16:35:15 +0100

    save

--- server/controllers/EmailTouser.js
+++ server/controllers/EmailTouser.js
@@ -27,6 +27,7 @@ transporter.verify(function(error, success) {
 
 
 const usermail =async (data, res) => {
+  console.log(data)
   var info = data.order.filter((el) => el.user_id === data.userID);
   var info_order = data.orderItems.filter((el) => el.order_id,info[0].id);
  
